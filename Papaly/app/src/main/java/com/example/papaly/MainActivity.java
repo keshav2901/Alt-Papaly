e9a39c2c213f25d5b700f1daf012cc3864d4a319
keshav2901 <89096717+keshav2901@users.noreply.github.com>
2022-03-03 22:18:18 +0530

    Keshav Commit activity main

--- Papaly/app/src/main/java/com/example/papaly/MainActivity.java
+++ Papaly/app/src/main/java/com/example/papaly/MainActivity.java
@@ -10,12 +10,22 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
     Button hi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(false)
+                    .add(R.id.fragment_container_view,  trial_fragment2.class, null)
+                    .addToBackStack("sum")
+                    .commit();
+        }
+
+
+
     }
 }
