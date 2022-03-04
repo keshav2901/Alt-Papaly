91cfd056c8b3245340adf994de1de5f050c1e1ca
M-Arvind <68342779+M-Arvind@users.noreply.github.com>
2022-03-04 12:44:18 +0530

    Authentication page added

--- Papaly/app/src/main/java/com/example/papaly/MainActivity.java
+++ Papaly/app/src/main/java/com/example/papaly/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.papaly;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.example.papaly.Authentication.Number;
+
+import com.example.papaly.Statistics.trial_fragment2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .setReorderingAllowed(false)
-                    .add(R.id.fragment_container_view,  trial_fragment2.class, null)
-                    .addToBackStack("sum")
-                    .commit();
-        }
+        FragmentTransaction fragment = getSupportFragmentManager().beginTransaction();
+        fragment.replace(R.id.Authentication, Number.class, null).commit();
 
 
 
